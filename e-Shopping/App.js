7b8428c6823951852043f3f019cde538faa20e7c
Xiaoping-Li <narci2008@gmail.com>
2019-06-04 10:42:30 -0700

    Init App.js

--- e-Shopping/App.js
+++ e-Shopping/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import EShopping from './src/EShopping';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <EShopping />
       </View>
     );
   }
