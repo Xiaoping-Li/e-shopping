568d9811b169612affe145b3f6e558d55dc65b62
Xiaoping-Li <narci2008@gmail.com>
2019-06-05 10:14:35 -0700

    Modify welcomeCarousel

--- e-Shopping/src/components/presentations/WelcomeCarousel.js
+++ e-Shopping/src/components/presentations/WelcomeCarousel.js
@@ -1,43 +1,64 @@
 import React from 'react';
-import { StyleSheet, View, Image } from 'react-native';
-import Carousel from 'react-native-carousel';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
 
 
 class WelcomeCarousel extends React.Component {
-  render() {
+  _renderItem = ({item, index}) => {
     return (
-      
+      <View style={styles.item}>
+          <Image source={item.img} style={{width: 300, height: 180, borderRadius: 5}}/>
+      </View>
+    );
+  }
+
+  render() {
+    const data = [
+      {
+        name: 'panda',
+        nums: 10,
+        img: require('../../../assets/photo/f-panda.png'),
+      },
+      {
+        name: 'snake',
+        nums: 20,
+        img: require('../../../assets/photo/r-green-snake.png'),
+      },
+      {
+        name: 'clownfish',
+        nums: 10,
+        img: require('../../../assets/photo/a-clownfish.png'),
+      },
+      {
+        name: 'cockatiel parrot',
+        nums: 10,
+        img: require('../../../assets/photo/b-cockatiel-parrot.png'),
+      },
+      {
+        name: 'red eyed tree frog',
+        nums: 10,
+        img: require('../../../assets/photo/r-red-eyed-tree-frog.png'),
+      },
+    ];
+    return (
+      <View style={styles.container}>
         <Carousel
-          width={375}
-          animate={true}
-          delay={3000}
+          ref={(c) => { this._carousel = c; }}
+          data={data}
+          renderItem={this._renderItem}
+          sliderWidth={300}
+          sliderHeight={250}
+          itemWidth={300}
+          itemHeight={200}
+          autoplay={true}
+          enableMomentum={false}
+          lockScrollWhileSnapping={true}
+          enableSnap={true}
           loop={true}
-          hideIndicators={false}
-          indicatorOffset={150}
-          indicatorAtBottom={true}
-          indicatorSize={15}
-        >
-          <View style={styles.container}>
-            <Image width={300} height={300} source={require('../../../assets/photo/f-panda.png')} />
-          </View>
-
-          <View style={styles.container}>
-            <Image width={300} height={300} source={require('../../../assets/photo/a-clownfish.png')} />
-          </View> 
-
-          <View style={styles.container}>
-            <Image width={300} height={300} source={require('../../../assets/photo/r-green-snake.png')} />
-          </View>
-
-          <View style={styles.container}>
-            <Image width={300} height={300} source={require('../../../assets/photo/b-cockatiel-parrot.png')} />
-          </View>
-
-          <View style={styles.container}>
-            <Image width={300} height={300} source={require('../../../assets/photo/r-red-eyed-tree-frog.png')} />
-          </View>
-        </Carousel>
-    
+          autoplayInterval={3000}
+          layout={'stack'}
+        />
+      </View>
     );
   }
 }
@@ -46,9 +67,20 @@ export default WelcomeCarousel;
 
 const styles = StyleSheet.create({
   container: {
-    width: 375,
+    width: 350,
+    height: 300,
+    backgroundColor: '#000',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'transparent',
+    borderRadius: 10,
   },
+  item: {
+    width: 300,
+    height: 200,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    borderRadius:  5,
+    marginTop: 30,
+  }
 });
--- e-Shopping/src/components/screens/WelcomeScreen.js
+++ e-Shopping/src/components/screens/WelcomeScreen.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { WelcomeCarousel } from '../presentations';
+
+
 
 
 class WelcomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.carouselContainer}>
+        <View >
           <WelcomeCarousel />
         </View>
         
@@ -31,20 +33,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center', 
     justifyContent: 'center',
-  },
-  carouselContainer: {
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    width: 375,
-    height: 375,
-    borderBottomColor: 'gray',
-    borderBottomWidth: 1,
-    // borderBottomEndRadius: 5,
-  },
-  carousel: {
-    width: 375,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'transparent',
   },
 });
