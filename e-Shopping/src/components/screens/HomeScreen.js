e3ab2f1174d8b0e218bb7d8887438e5d27de5e12
Xiaoping-Li <narci2008@gmail.com>
2019-06-05 15:44:33 -0700

    Add signout to home page

--- e-Shopping/src/components/screens/HomeScreen.js
+++ e-Shopping/src/components/screens/HomeScreen.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, Button, AsyncStorage, } from 'react-native';
 
 class HomeScreen extends React.Component {
+  signOut = async () => {
+    AsyncStorage.clear();
+    this.props.navigation.navigate('AuthLoading');
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Welcome to HomeScreen</Text>
+        <Button
+          title="Sign Out"
+          onPress={this.signOut}
+        />
       </View>
     );
   }
--- e-Shopping/src/components/screens/SignInScreen.js
+++ e-Shopping/src/components/screens/SignInScreen.js
@@ -1,13 +1,83 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { 
+    View, 
+    Text, 
+    StyleSheet, 
+    TextInput,
+    AsyncStorage, 
+    ActivityIndicator, 
+} from 'react-native';
+import { Font } from 'expo';
 
-class SignInScreen extends React.Component {
+
+class SignInScreen extends Component {
+  constructor() {
+      super();
+      this.state = {
+          email: '',
+          password: '',
+          fontLoaded: false,
+      }
+  }
+
+  // Make sure the font loaded before using
+  async componentDidMount() {
+      await Font.loadAsync({
+          'Pacifico-Reg': require('../../../assets/fonts/Pacifico-Regular.ttf'), 
+      });
+
+      this.setState({ fontLoaded: true });
+  }
+
+  signIn = async () => {
+    await AsyncStorage.setItem('userToken', 'xiaoping');
+    this.props.navigation.navigate('App');
+  }
+    
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Welcome to SignInScreen</Text>
-      </View>
-    );
+      return (
+          <View style={styles.container}>
+              {/* App Header */}
+              {this.state.fontLoaded ? (
+                  <Text style={styles.header}>Pets e-Shopping</Text>    
+              ) : (
+                  <ActivityIndicator size="large" />
+              )}
+
+              <View style={styles.divider_bar}></View>
+
+              {/* Sign In Form */}
+              <TextInput
+                  placeholder="Phone number or email"
+                  value={this.state.username}
+                  onChangeText={email => this.setState({email})}
+                  style={styles.input}
+              />
+
+              <TextInput
+                  placeholder="Password"
+                  value={this.state.password}
+                  onChangeText={password => this.setState({password})}
+                  style={styles.input}
+              />
+
+              {/* Sign In Button */}
+              <View style={styles.btn}>
+                  <Text
+                      onPress={this.signIn}
+                      accessibilityLabel="Sign In"
+                      style={styles.btnText}
+                  >
+                      Sign In
+                  </Text>
+
+                  <View style={styles.divider_bar}></View> 
+
+                  {/* Forgot Password link */}
+                  <Text>Forgot password?</Text>
+              </View> 
+          </View>
+      );
   }
 }
 
@@ -15,9 +85,52 @@ export default SignInScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      backgroundColor: '#fff',
+      alignItems: 'center', 
+      justifyContent: 'center',
+      width: 350,
+      marginLeft: 'auto',
+      marginRight: 'auto',
   },
+  header: {
+    fontFamily: 'Pacifico-Reg',
+    fontSize: 40,
+    marginBottom: 30, 
+    color: '#0E4375',
+    shadowColor: '#092E51',
+    shadowOffset: {width: 1, height: 1},
+    shadowOpacity: 0.8,
+    shadowRadius: 1,
+  },
+  divider_bar: {
+      width: 350,
+      backgroundColor: '#D3D3D3',
+      height: 1,
+      marginTop: 30,
+      marginBottom: 30,
+  },
+  input: {
+      borderWidth: 1,
+      borderColor: 'gray',
+      borderStyle: 'solid',
+      padding: 10,
+      width: 350,
+      marginBottom: 25,
+      borderRadius: 10,
+  },
+  btn: {
+      width: 350,
+      backgroundColor: '#009FF8',
+      marginTop: 25,
+      padding: 10,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 10,
+  },
+  btnText: {
+      textAlign: 'center',
+      fontSize: 20,
+      width: 300,
+      color: '#fff',
+  }
 });
