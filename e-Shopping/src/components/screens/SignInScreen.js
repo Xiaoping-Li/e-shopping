ebe82fe56dfb4caa11f3bff0b3fb37d6a3c2c458
Xiaoping-Li <narci2008@gmail.com>
2019-06-05 15:56:00 -0700

    Style for SignIn page

--- e-Shopping/src/components/screens/SignInScreen.js
+++ e-Shopping/src/components/screens/SignInScreen.js
@@ -38,29 +38,31 @@ class SignInScreen extends Component {
       return (
           <View style={styles.container}>
               {/* App Header */}
-              {this.state.fontLoaded ? (
-                  <Text style={styles.header}>Pets e-Shopping</Text>    
-              ) : (
-                  <ActivityIndicator size="large" />
-              )}
+              {this.state.fontLoaded ? 
+                <Text style={styles.header}>Pets e-Shopping</Text>    
+               : 
+                <ActivityIndicator size="large" />
+              }
 
               <View style={styles.divider_bar}></View>
 
               {/* Sign In Form */}
-              <TextInput
+              <View style={styles.form}>
+                <TextInput
                   placeholder="Phone number or email"
                   value={this.state.username}
                   onChangeText={email => this.setState({email})}
                   style={styles.input}
-              />
+                />
 
-              <TextInput
+                <TextInput
                   placeholder="Password"
                   value={this.state.password}
                   onChangeText={password => this.setState({password})}
                   style={styles.input}
-              />
-
+                />
+              </View>
+              
               {/* Sign In Button */}
               <View style={styles.btn}>
                   <Text
@@ -70,12 +72,12 @@ class SignInScreen extends Component {
                   >
                       Sign In
                   </Text>
+              </View>
 
-                  <View style={styles.divider_bar}></View> 
+              <View style={styles.divider_bar}></View> 
 
-                  {/* Forgot Password link */}
-                  <Text>Forgot password?</Text>
-              </View> 
+              {/* Forgot Password link */}
+              <Text>Forgot password?</Text>
           </View>
       );
   }
@@ -85,17 +87,17 @@ export default SignInScreen;
 
 const styles = StyleSheet.create({
   container: {
-      backgroundColor: '#fff',
-      alignItems: 'center', 
-      justifyContent: 'center',
-      width: 350,
-      marginLeft: 'auto',
-      marginRight: 'auto',
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center', 
+    justifyContent: 'center',
+    width: 350,
+    marginLeft: 'auto',
+    marginRight: 'auto',
   },
   header: {
     fontFamily: 'Pacifico-Reg',
-    fontSize: 40,
-    marginBottom: 30, 
+    fontSize: 40, 
     color: '#0E4375',
     shadowColor: '#092E51',
     shadowOffset: {width: 1, height: 1},
@@ -103,34 +105,44 @@ const styles = StyleSheet.create({
     shadowRadius: 1,
   },
   divider_bar: {
-      width: 350,
-      backgroundColor: '#D3D3D3',
-      height: 1,
-      marginTop: 30,
-      marginBottom: 30,
+    width: 350,
+    backgroundColor: '#FAD9C5',
+    height: 1,
+    marginTop: 50,
+    marginBottom: 30,
+  },
+  form: {
+    width: 350,
+    height: 250,
+    alignItems: 'center', 
+    justifyContent: 'center',
+
   },
   input: {
-      borderWidth: 1,
-      borderColor: 'gray',
-      borderStyle: 'solid',
-      padding: 10,
-      width: 350,
-      marginBottom: 25,
-      borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderStyle: 'solid',
+    padding: 10,
+    width: 350,
+    marginBottom: 25,
+    borderRadius: 10,
   },
   btn: {
-      width: 350,
-      backgroundColor: '#009FF8',
-      marginTop: 25,
-      padding: 10,
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderRadius: 10,
+    width: 280,
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F08E52',
+    borderRadius: 10,
+    shadowColor: '#2F1A0C',
+    shadowOffset: {width: 1, height: 1},
+    shadowOpacity: 1,
+    shadowRadius: 3,
   },
   btnText: {
-      textAlign: 'center',
-      fontSize: 20,
-      width: 300,
-      color: '#fff',
+    textAlign: 'center',
+    fontSize: 20,
+    width: 300,
+    color: '#fff',
   }
 });
