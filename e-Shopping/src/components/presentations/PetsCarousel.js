f1ecc82cd6a308e64d842faa670fa15a33d50ca7
Xiaoping-Li <narci2008@gmail.com>
2019-06-07 11:59:25 -0700

    PetsFlatList FlatList passdown active slide index to MyModal

---
+++ e-Shopping/src/components/presentations/PetsCarousel.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { StyleSheet, View, Image, Text } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
+
+
+class PetsCarousel extends React.Component {
+  _renderItem = ({item, index}) => {
+    return (
+      <View style={styles.item}>
+          <Image source={item.img} style={styles.img}/>
+          <Text>{item.name}</Text>
+      </View>
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Carousel
+          ref={(c) => { this._carousel = c; }}
+          data={this.props.data}
+          renderItem={this._renderItem}
+          sliderWidth={250}
+          sliderHeight={250}
+          itemWidth={250}
+          itemHeight={200}
+          enableMomentum={false}
+          lockScrollWhileSnapping={true}
+          enableSnap={true}
+          layout={'stack'}
+        />
+      </View>
+    );
+  }
+}
+
+export default PetsCarousel;
+
+const styles = StyleSheet.create({
+  container: {
+    width: 280,
+    height: 230,
+    backgroundColor: '#EF823F',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    shadowColor: '#2F1A0C',
+    shadowOffset: {width: 1, height: 1},
+    shadowOpacity: 1,
+    shadowRadius: 3,
+  },
+  item: {
+    width: 250,
+    height: 190,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    borderRadius:  5,
+    marginTop: 15,
+  },
+  img: {
+    width: 240, 
+    height: 180, 
+    marginTop: 5,
+    borderRadius: 5,
+  },
+});
--- e-Shopping/src/components/presentations/PetsFlatList.js
+++ e-Shopping/src/components/presentations/PetsFlatList.js
@@ -8,41 +8,43 @@ class PetsFlatList extends PureComponent {
         super();
         this.state = {
             visible: false,
+            activeIdx: 0,
         };
     }
 
-    toggleVisible = () => this.setState({ visible: !this.state.visible})
+    toggleVisible = (index) => this.setState({ visible: !this.state.visible, activeIdx: index})
 
     render() {
         return (
             <View style={styles.container}>
                 <FlatList
                     data={this.props.data}
-                    renderItem={({item}) => {
+                    renderItem={({item, index}) => {
                         return (
                             <TouchableOpacity
                                 style={[styles.card, styles.shadow]}
-                                onPress={this.toggleVisible}
+                                onPress={() => this.toggleVisible(index)}
                             >
                                 <Image source={item.img} style={styles.img}/>
-                                <Text style={styles.text}>{item.name}</Text>
-                            </TouchableOpacity>
+                                <Text style={styles.text}>{item.name}</Text>                              
+                            </TouchableOpacity>   
                         );
                     }}
                     keyExtractor={(item) => item._id}
                     horizontal={false}
                     numColumns={2}
-                />
+                /> 
 
                 <MyModal visible={this.state.visible}>
                     <View style={{width: 300, height: 100}}>
-                       <Text onPress={this.toggleVisible} style={{fontSize: 20, color: '#fff'}}>I am here</Text> 
+                    <Text onPress={this.toggleVisible} style={{fontSize: 20, color: '#fff'}}>I am here{this.state.activeIdx}</Text> 
                     </View>
                 </MyModal>
             </View>
         );
     }
 }
+
 
 export default PetsFlatList;
 
