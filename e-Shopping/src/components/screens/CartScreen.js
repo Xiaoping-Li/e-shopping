fb42bd96da8453226d94fbc1ede20c9aeff02ce6
Xiaoping-Li <narci2008@gmail.com>
2019-06-10 12:07:06 -0700

    Shopping Cart style

--- e-Shopping/src/components/screens/CartScreen.js
+++ e-Shopping/src/components/screens/CartScreen.js
@@ -15,12 +15,32 @@ class CartScreen extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.header}>Shopping Cart</Text>
-        {cart.length ? 
-          <FlatList
-            data={cart}
-            keyExtractor={(item) => item._id} 
-            renderItem={({item}) => <CartItem pet={item} />}
-          />
+        {cart.length ?
+          <View>
+            <View style={styles.list}>
+              <FlatList
+                data={cart}
+                keyExtractor={(item) => item._id} 
+                renderItem={({item}) => <CartItem pet={item} />} 
+              />
+            </View>
+            <Text style={styles.subTotal}>Subtotal (1 item): <Text style={{color: 'red'}}>$15.99</Text></Text>
+            <View style={styles.btnContainer}>
+              <TouchableOpacity
+                onPress={this.navigateToHome}
+                style={styles.btn}
+              >
+                <Text style={{color: '#0E4375', fontWeight: '600', fontSize: 20}}>Shopping</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                onPress={() => {}}
+                style={styles.btn}
+              >
+                <Text style={{color: '#0E4375', fontWeight: '600', fontSize: 20}}>Checkout</Text>
+              </TouchableOpacity>
+            </View>
+          </View>  
           :
           <TouchableOpacity
             onPress={this.navigateToHome}
@@ -43,6 +63,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
+
+  // Empty shopping cart
   empty: {
     fontSize: 20,
     width: 300,
@@ -54,8 +76,9 @@ const styles = StyleSheet.create({
     fontSize: 30,
     color: '#0E4375',
     textAlign: 'left',
-    marginTop: 30,
-    marginBottom: 30,
+    marginTop: 20,
+    marginBottom: 20,
+    fontWeight: '600',
   },
   shopping: {
     color: '#F5C851',
@@ -64,39 +87,76 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     fontSize: 30,
   },
+
+  // FlatList style
+  list: {
+    borderTopColor: '#3E6890',
+    borderBottomColor: '#3E6890',
+    borderStyle: 'solid',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    width: 300,
+    height: 380,
+  },
+  subTotal: {
+    marginTop: 10,
+    width: 300,
+    textAlign: 'right',
+    fontSize: 20,
+    color: '#0E4375',
+  },
+  btnContainer: {
+    width: 300,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection:'row',
+    marginTop: 30,
+  },
+  btn: {
+    width: 140,
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F5C851',
+    borderRadius: 10,
+    shadowColor: '#312810',
+    shadowOffset: {width: 1, height: 1},
+    shadowOpacity: 1,
+    shadowRadius: 3,
+  }
 });
 
 const cart = [
-  // {
-  //   _id: '5793f6de-8948-11e9-bc42-526af7764f64',
-  //   name: 'Cockatiel Parrot',
-  //   img: require('../../../assets/photo/b-cockatiel-parrot.png'),
-  //   desc: 'Choosing Anime produces unusual hair and eye colours, and choosing Unique produces unusual hair, eye AND skin colours. Choose simple or detailed for basic or more inclusive results respectively.',
-  //   count: 5,
-  //   price: 20,
-  // },
-  // {
-  //   _id: '5793f936-8948-11e9-bc42-526af7764f64',
-  //   name: 'Hummingbird',
-  //   img: require('../../../assets/photo/b-hummingbird.png'),
-  //   desc: 'Choosing Anime produces unusual hair and eye colours, and choosing Unique produces unusual hair, eye AND skin colours. Choose simple or detailed for basic or more inclusive results respectively.',
-  //   count: 0,
-  //   price: 20,
-  // },
-  // {
-  //   _id: '5794005c-8948-11e9-bc42-526af7764f64',
-  //   name: 'Alpaca',
-  //   img: require('../../../assets/photo/f-alpaca.png'),
-  //   desc: 'Choosing Anime produces unusual hair and eye colours, and choosing Unique produces unusual hair, eye AND skin colours. Choose simple or detailed for basic or more inclusive results respectively.',
-  //   count: 10,
-  //   price: 20,
-  // },
-  // {
-  //   _id: '5793fc9c-8948-11e9-bc42-526af7764f66',
-  //   name: 'Koala',
-  //   img: require('../../../assets/photo/f-koala.png'),
-  //   desc: 'Choosing Anime produces unusual hair and eye colours, and choosing Unique produces unusual hair, eye AND skin colours. Choose simple or detailed for basic or more inclusive results respectively.',
-  //   count: 10,
-  //   price: 20,
-  // },
+  {
+    _id: '5793f6de-8948-11e9-bc42-526af7764f64',
+    name: 'Cockatiel Parrot',
+    img: require('../../../assets/photo/b-cockatiel-parrot.png'),
+    desc: 'Choosing Anime produces unusual hair and eye colours, and choosing Unique produces unusual hair, eye AND skin colours. Choose simple or detailed for basic or more inclusive results respectively.',
+    count: 5,
+    price: 20,
+  },
+  {
+    _id: '5793f936-8948-11e9-bc42-526af7764f64',
+    name: 'Hummingbird',
+    img: require('../../../assets/photo/b-hummingbird.png'),
+    desc: 'Choosing Anime produces unusual hair and eye colours, and choosing Unique produces unusual hair, eye AND skin colours. Choose simple or detailed for basic or more inclusive results respectively.',
+    count: 0,
+    price: 20,
+  },
+  {
+    _id: '5794005c-8948-11e9-bc42-526af7764f64',
+    name: 'Alpaca',
+    img: require('../../../assets/photo/f-alpaca.png'),
+    desc: 'Choosing Anime produces unusual hair and eye colours, and choosing Unique produces unusual hair, eye AND skin colours. Choose simple or detailed for basic or more inclusive results respectively.',
+    count: 10,
+    price: 20,
+  },
+  {
+    _id: '5793fc9c-8948-11e9-bc42-526af7764f66',
+    name: 'Koala',
+    img: require('../../../assets/photo/f-koala.png'),
+    desc: 'Choosing Anime produces unusual hair and eye colours, and choosing Unique produces unusual hair, eye AND skin colours. Choose simple or detailed for basic or more inclusive results respectively.',
+    count: 10,
+    price: 20,
+  },
 ];
