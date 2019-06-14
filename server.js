a8f428fe9e7ce76ad8b96fa3bde90329ae554d75
Xiaoping-Li <narci2008@gmail.com>
2019-06-13 17:07:28 -0700

    finish

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
 const STRIPE_SECRET_KEY = require('./config/keys_dev').STRIPE_SECRET_KEY;
+const stripe = require('stripe')(STRIPE_SECRET_KEY);
+
 const PORT = process.env.PORT || 5000;
 
 const server = express();
@@ -12,5 +13,10 @@ server.use(cors({
     origin: true,
     credentials: true,
 }));
+
+// Charge customer with token
+server.post('/payment', (req, res) => {
+    return stripe
+})
 
 server.listen(PORT, () => console.log(`server listen on ${PORT}`));
