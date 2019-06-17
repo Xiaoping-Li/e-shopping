46abefd8291048f5c7f0feeb079483b16fbe6e84
Xiaoping-Li <narci2008@gmail.com>
2019-06-17 15:31:36 -0700

    Sign In user

--- server.js
+++ server.js
@@ -27,21 +27,47 @@ server.use(cors({
     origin: true,
     credentials: true,
 }));
+server.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+}));
 
 // Sign Up User
 server.post('/signup', (req, res) => {
-    bcrypt.hash(req.body.password, 11, (err, hashPW) => {
+    bcrypt.hash(req.body.password, 11, (err, hashedPW) => {
         if (err) {
             res.status(422).json({"error": err});
         } else {
             const user = req.body;
-            user.password = hashPW;
+            user.password = hashedPW;
             Users
                 .create(user)
                 .then(result => res.status(200).json({success: true, result}))
                 .catch(err => console.log(err));
         }
     });
+});
+
+// Sign In User
+server.post('/signin', (req, res) => {
+    const { email } = req.body;
+    Users
+        .findOne({ email })
+        .then(user => {
+            const hashedPW = user.password;
+            bcrypt
+                .compare(req.body.password, hashedPW)
+                .then(result => {
+                    if (!result) throw new Error();
+                    req.session.email = req.body;
+                    req.user = user;
+                    delete req.user.password;
+                    res.json({success: true, user: req.user});
+                })
+                .catch(err => res.json({msg: "Failed when comparing password"}));
+        })
+        .catch(err => res.json({msg: "Failed to find the user"}));
 });
 
 // Using Routers
