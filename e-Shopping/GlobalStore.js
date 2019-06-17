f9fd87f3d1ca0023fc7fa81395994b2620bbc56c
Xiaoping-Li <narci2008@gmail.com>
2019-06-17 09:47:25 -0700

    Update GlobalStore

--- e-Shopping/GlobalStore.js
+++ e-Shopping/GlobalStore.js
@@ -17,5 +17,32 @@ class GlobalStore {
     reptile = [];
     initAquarium = list => this.aquarium = list;
     initBird = list => this.bird = list;
-    initFluffy = list => this.fluffy = list;  
+    initFluffy = list => this.fluffy = list;
+    initReptile = list => this.reptile = list;
+    updateAquariumCount = (update, idx) => this.aquarium[idx].count = update; 
+    updateBirdCount = (update, idx) => this.bird[idx].count = update;
+    updateFluffyCount = (update, idx) => this.fluffy[idx].count = update;
+    updateReptileCount = (update, idx) => this.reptile[idx].count = update; 
 }
+
+decorate(
+    GlobalStore,
+    {
+        user: observable,
+        aquarium: observable,
+        bird: observable,
+        fluffy: observable,
+        reptile: observable,
+        updateUser: action,
+        initAquarium: action,
+        initBird: action,
+        initFluffy: action,
+        initReptile: action,
+        updateAquariumCount: action,
+        updateBirdCount: action,
+        updateFluffyCount: action,
+        updateReptileCount: action,
+    }
+);
+
+export default new GlobalStore();
