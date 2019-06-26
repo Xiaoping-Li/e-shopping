78b86821ef0120af645f6ff25a74f80110bb80c5
Xiaoping-Li <narci2008@gmail.com>
2019-06-26 11:11:50 -0700

    SendPWEmailScreen style done

--- e-Shopping/src/components/screens/sendPWEmailScreen.js
+++ e-Shopping/src/components/screens/sendPWEmailScreen.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { 
+  StyleSheet, 
+  View, 
+  Text, 
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+} from 'react-native';
+import Icon from '@expo/vector-icons/FontAwesome';
 
 class SendPWEmailScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: ''
+    };
+  }
+
   render() {
     return (
-      <View>
-        <Text>SendPWEmailScreen Page</Text>
-      </View>
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <Text style={styles.header}>Please enter your register email below. A reset password email will be sent to you.</Text>
+        <View style={styles.sendContainer}>
+          <TextInput
+            placeholder="Register email"
+            value={this.state.email}
+            onChangeText={email => this.setState({email})}
+            style={[styles.input, styles.shadow]}
+          />
+
+          <TouchableOpacity onPress={() => {}}>
+            <Icon
+              color="#F08E52"
+              name="send"
+              size={30} 
+              style={styles.shadow}
+            />
+          </TouchableOpacity>  
+        </View>  
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -20,4 +52,29 @@ const styles = StyleSheet.create({
     alignItems: 'center', 
     justifyContent: 'center',
   },
+  header: {
+    fontSize: 20, 
+    color: '#0E4375',
+  },
+  sendContainer: {
+    flexDirection: 'row',
+    alignItems: 'center', 
+    justifyContent: 'center',
+    height: 300,
+  },
+  input: {
+    borderWidth: 2,
+    borderColor: '#F08E52',
+    fontSize: 20,
+    padding: 5,
+    width: 300,
+    marginRight: 10,
+    borderRadius: 10,
+  },
+  shadow: {
+    shadowColor: '#2F1A0C',
+    shadowOffset: {width: 0.5, height: 0.5},
+    shadowOpacity: 1,
+    shadowRadius: 0.5,
+  }
 });
