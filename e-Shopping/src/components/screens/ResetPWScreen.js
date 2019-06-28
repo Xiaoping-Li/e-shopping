97e5f24ecc224d51ac171939fa7e7199aa28b211
Xiaoping-Li <narci2008@gmail.com>
2019-06-28 13:48:13 -0700

    ResetPWScreen loaded as expected

--- e-Shopping/src/components/screens/ResetPWScreen.js
+++ e-Shopping/src/components/screens/ResetPWScreen.js
@@ -6,11 +6,12 @@ import {
   TextInput,
   TouchableOpacity,
   KeyboardAvoidingView,
-  // Platform, 
+  Platform, 
   Linking,
 } from 'react-native';
 import Icon from '@expo/vector-icons/FontAwesome';
 import axios from 'axios';
+
 
 
 class ResetPWScreen extends Component {
@@ -21,31 +22,79 @@ class ResetPWScreen extends Component {
       email: '',
       password: '',
       rePassword: '',
-    };
+      token: '',
+    };  
   }
 
   componentDidMount = () => {
-    Linking.addEventListener('url', this.handleOpenURL);
+    // if (Platform.OS === 'android') {
+      Linking
+        .getInitialURL()
+        .then(url => {
+          const token = url.slice(44);
+          axios
+            .get(`http://192.168.0.107:5000/reset_password?token=${token}`)
+            .then(result => {
+              if (!result.data.success) {
+                alert(`${result.data.msg}`);
+                this.props.navigation.navigate("SendEmail");
+              } else {
+                this.setState({username: result.data.username, email: result.data.email, token });
+              }
+            })
+            .catch(err => console.log("Error when validate reset token: " + err)); 
+        });
+    // } else {
+    //   Linking.addEventListener('url', this.handleOpenURL);
+    // } 
+    
   }
 
-  componentWillUnmount = () => {
-    Linking.removeEventListener('url', this.handleOpenURL);
-  }
+  // componentWillUnmount = () => {
+  //   Linking.removeEventListener('url', this.handleOpenURL);
+  // }
 
-  handleOpenURL = (event) => {
-    // const result = Expo.Linking.parse(event.url);
-    console.log(event.url);
-    console.log(event);
-  }
+  // handleOpenURL = (event) => {
+  //   const token = event.url.slice(44);
+  //   this.setState({ token});
+  //   // axios
+  //   //   .get(`http://192.168.0.107:5000/reset_password?token=${token}`)
+  //   //   .then(result => {
+  //   //     if (!result.data.success) {
+  //   //       alert(`${result.data.msg}`);
+  //   //       this.props.navigation.navigate("SendEmail");
+  //   //     } else {
+  //   //       this.setState({username: result.data.username, email: result.data.email, token });
+  //   //     }
+  //   //   })
+  //   //   .catch(err => console.log("Error when validate reset token: " + err));
+  // }
 
-  navigateToSignIn = () => this.props.navigation.navigate("SignIn");
+  resetPW = () => {
+    const info = {
+      token: this.state.token,
+      newPW: this.state.password,
+    };
+
+    axios
+      .put(`http://192.168.0.107:5000/reset_password`, info)
+      .then(result => {
+        if (result.data.resetPW) {
+          alert("Congras! You have successfully reset your password. Please sign in with the new password!");
+          this.props.navigation.navigate("SignIn");
+        } else {
+          alert("Sorry! Some thing wrong when saving the new password. Please retry");
+        }
+      })
+      .catch(err => console.log("Error when saving the new password: " + err));
+  }
 
   render() {
     return (
       <KeyboardAvoidingView style={styles.container} behavior="padding">
         <View>
           <Text style={styles.header}>Dear {this.state.username}:</Text>
-          <Text>Welcome back! Please reset your password.</Text>
+          <Text>Welcome back! Please reset your password. And the register email for reseting password is: {this.state.email}</Text>
         </View>
         
         <View style={styles.sendContainer}>
@@ -63,7 +112,7 @@ class ResetPWScreen extends Component {
             style={[styles.input, styles.shadow]}
           />
 
-          <TouchableOpacity onPress={this.navigateToSignIn}>
+          <TouchableOpacity onPress={this.resetPW}>
             <Icon
               color="#F08E52"
               name="send"
@@ -71,9 +120,7 @@ class ResetPWScreen extends Component {
               style={styles.shadow}
             />
           </TouchableOpacity>    
-        </View> 
-
-        {/* <Text>{this.props.navigation.state.params}</Text> */}
+        </View>
       </KeyboardAvoidingView>
     );
   }
